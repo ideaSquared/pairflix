3e48e5a2505a7412bb5e359972bb4739c7b44796
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2025-06-10 20:44:22 +0100

    Fix lib.components tests - Add TextEncoder polyfill for CI compatibility

--- lib.components/setupTests.tsx
+++ lib.components/setupTests.tsx
@@ -1,3 +1,8 @@
+// Polyfill for TextEncoder and TextDecoder (needed for modern browser APIs)
+import { TextDecoder, TextEncoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
